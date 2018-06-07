16b4224865ff048a56355fb8b440ce4327d6627f
wwiras <wwiras@gmail.com>
2018-06-07 10:36:58 +0800

    Templates added

--- staff/views.py
+++ staff/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Staff
 
 # Create your views here.
+
+def home(request):
+	# return render(request,'base.html')
+	return render(request,'staff/base.html')
